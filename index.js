2cd058eba61257e5aa8dd2177c838738f65aa3bb
Joshua <yoonj200@gmail.com>
2022-02-16 17:50:44 -0600

    commit tree a7c65c09b3ab03ec6ef1e7533e5109f67320ecd2

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
+// Write your algorithm here
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  
+  for (let i = 0; i < array.length; i++) {
+  //indexOf() => return first index at which an element can be found in array, 
+  //  or -1 if it is not present
+    const diffIndex = array.indexOf(target - array[i]);
+    if (diffIndex >= 0 && diffIndex !== i) {
+      return true;
+    }
+  }
+  return false; // no solution found
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+O(1)
+
+O(1) = Constant (ex: Accessing a value in an object by its key)
+O(log n) = Logarithmic (ex: Binary search - more on this soon)
+O(n) = Linear (ex: Iterating through every element in an array)
+O(n²) = Quadratic	(ex: Nested iteration)
 */
 
 /* 
   Add your pseudocode here
+
+  iterate through array from beginning, 1 by 1 until the end
+    declare diffIndex as difference between target & index value
+    //if diffIndex greater than/equal to 0 and not equal to itself
+      return true
+  return false
+
 */
 
 /*
